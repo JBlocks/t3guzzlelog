bfc451fcbdb8b6555b4da31925de1b3e0594a79e
mabolek <mathias@lilio.com>
2020-06-08 11:51:10 +0200

    [BUGFIX] Handler cannot be array

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,7 @@ defined('TYPO3_MODE') or die();
 
 (static function (): void {
 
-    $GLOBALS['TYPO3_CONF_VARS']['HTTP']['handler'][] =
+    $GLOBALS['TYPO3_CONF_VARS']['HTTP']['handler'] =
         (\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\JBLOCKS\T3Guzzlelog\Middleware\Guzzle\ClientLogger::class))->handler();
 
 })();
